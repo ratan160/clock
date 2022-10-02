426f2add0b2c551b02471e63f07f204d17c75e21
Mahesh Suthar <mrsuthar210@gmail.com>
2022-10-02 15:53:55 +0530

    some feature of digital clock added

--- clockdigital.cpp
+++ clockdigital.cpp
@@ -1,30 +1,90 @@
+// C++ program to illustrate the digital
+// clock starting from the entered time
+
+#include <iomanip>
 #include <iostream>
+#include <stdlib.h>
+#include <windows.h>
 using namespace std;
 
 int main()
 {
-    int h, m, s;
-    cout << "enter the following data " << endl;
-    cin >> h >> m >> s;
-    for (h; h < 24; h++)
+    system("color 30");
+
+    // Background color and Foreground
+    int hour, min, sec;
+    cout << setw(70)
+         << "*Enter the Current time*\n";
+
+    // Use of manipulator for taking
+    // input from the user
+    cout << "HH - ";
+    cin >> hour;
+    cout << "MM -  ";
+    cin >> min;
+    cout << "SS - ";
+    cin >> sec;
+
+    // Background color and the
+    // Foreground for 2nd screen
+    system("color 5F");
+
+    // Cases for the Wrong Time Input
+    if (hour > 23)
     {
-        for (m; m <= 59; m++)
-        {
-            for (s; s <= 59; s++)
-            {
-                cout << h << m << s;
-                if (h < 12)
-                {
-                    cout << " AM" << endl;
-                }
-                else
-                {
-                    cout << " PM" << endl;
-                }
-            }
-            s = 0;
-        }
-        m = 0;
+        cout << "Wrong Time input";
     }
-    return 0;
+    else if (min > 60)
+    {
+        cout << "Wrong Time Input";
+    }
+    else if (sec > 60)
+    {
+        cout << "Wrong Time Input";
+    }
+
+    // Otherwise
+    else
+    {
+        while (1)
+
+        // Run Block infinitely
+        {
+            system("cls");
+
+            // Clear the console
+
+            // Code for Showing Time
+            for (hour; hour < 24; hour++)
+            {
+
+                for (min; min < 60; min++)
+                {
+
+                    for (sec; sec < 60; sec++)
+                    {
+                        system("cls");
+
+                        cout << "\n\n\n\n***************"
+                                "************************"
+                                "*********************** "
+                                "Current Time = "
+                             << hour << ":" << min << ":"
+                             << sec
+                             << "Hrs ********************"
+                                "**********************"
+                                "**********************";
+
+                        // HH:MM:SS columns in output
+
+                        Sleep(1000);
+
+                        // Pause for 1 sec
+                    }
+                    sec = 0;
+                }
+                min = 0;
+            }
+        }
+    }
 }
